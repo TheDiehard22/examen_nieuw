0e4c6bd4f8765e2f0d8f5ef8ffd13dc32b5a6744
Justin Breg <Justin.hallo@hotmail.com>
2017-05-09 20:49:48 +0200

    Add project files.

---
+++ Examen_nieuw/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen_nieuw.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
